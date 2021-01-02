d9e6b2cd2edec3ea024293120c67d4a777d603df
dan_bibibi <dlaeksql08222@gmail.com>
2021-01-03 02:10:56 +0900

    피보나치 수 5

--- P10870.c
+++ P10870.c
@@ -2,6 +2,5 @@
 int main(){
     int fibo[21]={}; fibo[0]=0; fibo[1]=1;
     for(int i=2; i<21; i++) fibo[i]=fibo[i-1]+fibo[i-2];
-    int N; scanf("%d", &N); 
-    printf("%d", fibo[N]);
+    int N; scanf("%d", &N); printf("%d", fibo[N]);
 }
