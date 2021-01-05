d00349edf125663ae640459d5844e81dc1002d0d
dan_bibibi <dlaeksql08222@gmail.com>
2021-01-06 04:35:02 +0900

    피보나치 수4

---
+++ P10826.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+string add(string str1, string str2){
+    string ans=""; int idx1= str1.size()-1, idx2=str2.size()-1, tmp=0, carry=0;
+    while (idx1>=0 && idx2>=0){
+        tmp = (str1.at(idx1)-48) + (str2.at(idx2)-48) + carry; carry=0;
+        if(tmp>9){
+            ans.insert(0, to_string((tmp+carry)%10)); carry=1;
+        }
+        else ans.insert(0, to_string(tmp+carry));
+        idx1--; idx2--;
+    }
+    if(idx1<0){
+        while (idx2>=0){
+            tmp = carry + (str2.at(idx2)-48); carry=0;
+            if(tmp>9){
+                ans.insert(0, to_string((tmp+carry)%10)); carry=1;
+            }
+            else ans.insert(0, to_string(tmp+carry));
+            idx2--;
+        }
+    }
+    else if(idx2<0){
+        while (idx1>=0){
+            tmp = carry + (str1.at(idx1)-48); carry=0;
+            if(tmp>9){
+                ans.insert(0, to_string((tmp+carry)%10)); carry=1;
+            }
+            else ans.insert(0, to_string(tmp+carry));
+            idx1--;
+        }
+    }
+    if(carry!=0) ans.insert(0, to_string(carry));
+    return ans;
+}
+
+int main() {
+    string fibo[10001]; fibo[0]="0"; fibo[1]="1";
+    int n; cin >> n;
+    for(int i=2; i<=n; i++){
+        fibo[i] = add(fibo[i-1], fibo[i-2]);
+    }
+    cout << fibo[n];
+}
