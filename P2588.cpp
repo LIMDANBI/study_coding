27426f70854e0e3d3995f3547093deea2fde5573
dan_bibibi <dlaeksql08222@gmail.com>
2021-01-15 15:00:40 +0900

    곱셉

---
+++ P2588.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[7]={};
+    cin >> arr[1] >> arr[2];
+    arr[3]=arr[1]*((arr[2]%100)%10); arr[4]=arr[1]*((arr[2]%100)/10); arr[5]=arr[1]*(arr[2]/100); arr[6]=arr[1]*arr[2];
+    for(int i=3; i<7; i++) cout << arr[i] << "\n";
+}
