a5b9240c1f651def18bb00ac1fdcbac4dcec413f
dan_bibibi <dlaeksql08222@gmail.com>
2021-01-23 17:17:07 +0900

    별 찍기 - 20(구현)

---
+++ P10995.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n; cin>>n;
+    for(int i=0; i<n; i++){
+        if(i%2==0){ for(int j=0; j<2*n-1; j++) j%2==0?cout<<"*":cout<<" ";}
+        else{ for(int j=0; j<2*n; j++) j%2==0?cout<<" ":cout<<"*";} cout<<'\n';
+    }
+}
