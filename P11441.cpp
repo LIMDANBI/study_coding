697f9f15f18fc94ad09b1ee452341302cc3dd8ff
dan_bibibi <dlaeksql08222@gmail.com>
2021-01-23 18:39:25 +0900

    합 구하기(누적 합)(최적화)

--- P11441.cpp
+++ P11441.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 int main(){
-    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
     int n, m, a, b, tmp, ans; cin>>n; int num[n+1]={0};
     for(int i=1; i<=n; i++) {
         cin>>tmp; num[i]=num[i-1]+tmp;
