b18fbc53157f27f238b692e83839a0690d8c78b2
dan_bibibi <dlaeksql08222@gmail.com>
2021-01-26 01:56:02 +0900

    세수정렬(정렬)

---
+++ P2752.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int num[3]; for(int i=0; i<3; i++) cin>>num[i];
+    sort(num+0, num+3); cout<<num[0]<<" "<<num[1]<<" "<<num[2];
+}
