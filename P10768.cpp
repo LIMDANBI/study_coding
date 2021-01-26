2c6e6a6cbeb04bef98a53ea26f03f8ec654ab76f
dan_bibibi <dlaeksql08222@gmail.com>
2021-01-26 21:02:49 +0900

    특별한 날(구현)

---
+++ P10768.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m, d; cin>>m>>d;
+    if(m==2 && d==18) cout<<"Special";
+    else{
+        if(m<2 || m==2&&d<18) cout<<"Before";
+        else cout<<"After";
+    }
+}
