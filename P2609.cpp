0a7940ddfcd63ede6fbe76109b3c2276c0e22599
dan_bibibi <dlaeksql08222@gmail.com>
2021-01-29 17:10:30 +0900

    최대공약수와 최소공배수 (수학/정수론/유클리드 호제법)

--- P2609.cpp
+++ P2609.cpp
@@ -5,8 +5,5 @@ int main(){
     if(n<m){a=m; b=n;} else {a=n; b=m;}
     while (a%b!=0){
         tmp=b; b=a%b; a=tmp;
-    } gcd=b;
-    if(n<m){a=m; b=n; lcm=m;} else {a=n; b=m; lcm=n;}
-    while(lcm%a!=0||lcm%b!=0) lcm+=gcd;
-    cout<<gcd<<'\n'<<lcm;
+    } gcd=b; lcm=n*m/b; cout<<gcd<<'\n'<<lcm;
 }
