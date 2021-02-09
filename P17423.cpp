8a0799a7e32596e52ae83b2a4512ac1dadaded3b
dan_bibibi <dlaeksql08222@gmail.com>
2021-02-10 06:48:35 +0900

    단어 뒤집기 2(구현/문자열)

---
+++ P17423.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+    stack<char> st; string str; getline(cin,str); int whole=0;
+    for(int i=0; i<str.size(); i++){
+        if(str[i]=='<') {
+            while (!st.empty()){ char tmp=st.top(); cout<<tmp; st.pop();}
+            cout<<str[i]; whole=1;}
+        else if(str[i]=='>') {cout<<str[i]; whole=0;}
+        else{
+            if(whole) cout<<str[i];
+            else{
+                if(str[i]==' ') {
+                    while (!st.empty()){
+                        char tmp=st.top(); cout<<tmp; st.pop();
+                    } cout<<' ';
+                } else st.push(str[i]);
+            }
+        }
+    } while (!st.empty()){ char tmp=st.top(); cout<<tmp; st.pop();} 
+}
