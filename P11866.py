8b58dc08a34fa5c19f60fd8581ba8a3ae4b9ef28
danbibibi <dlaeksql08222@gmail.com>
2021-09-17 02:38:45 +0900

    요세푸스 문제 0 (구현/자료구조/큐)

--- P11866.py
+++ P11866.py
@@ -1,11 +1,10 @@
 N, K = map(int, input().split())
-round_table = [i for i in range(1,N+1)]
+round_table = list(range(1, N+1))
 idx=0
 print('<', end='')
 while len(round_table)!=0:
     if len(round_table)!=N:
         print(', ', end='')
-    idx+=K-1
-    idx %= len(round_table)
+    idx = (idx + K - 1) % len(round_table)
     print(round_table.pop(idx), end='')
 print('>', end='')
