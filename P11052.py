d31075003563e5324ec7bffdb7f748f61dd796a7
danbibibi <dlaeksql08222@gmail.com>
2021-09-21 21:37:54 +0900

    카드 구매하기(다이나믹 프로그래밍)

---
+++ P11052.py
@@ -0,0 +1,13 @@
+N = int(input())
+card_pack = list(map(int, input().split()))
+ans = 0
+dp = [card_pack[0], card_pack[0]]
+for i in range(2, N+1):
+    tmp=0
+    for j in range(1, i//2+1):
+        if i%j==0:
+            tmp = max(tmp, dp[j] + dp[i-j], dp[j]*(i//j))
+        else:
+            tmp = max(tmp, dp[j]+dp[i-j])
+    dp.append(max(tmp, card_pack[i-1]))
+print(dp[N])
