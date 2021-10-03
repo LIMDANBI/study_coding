428e7d23229b8f9132f5dde06c2d059cd98c23ee
danbibibi <dlaeksql08222@gmail.com>
2021-10-04 06:33:31 +0900

    달팽이는 올라가고 싶다(수학)

--- P2869.py
+++ P2869.py
@@ -1,8 +1,6 @@
 def solution():
     import sys
     A, B, V = map(int, sys.stdin.readline().rstrip().split())
-    if V<=A: print(1)
-    else:
-        if (V-A)/(A-B) == 0 or (V-A)%(A-B)>0: print((V-A)//(A-B)+2)
-        else: print((V-A)//(A-B)+1)
+    if (V-A)%(A-B)!=0: print((V-A)//(A-B)+2)
+    else: print((V-A)//(A-B)+1)
 solution()
