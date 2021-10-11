cd15fe5c59a7d773c46acb58d2baf862bcad5072
danbibibi <dlaeksql08222@gmail.com>
2021-10-12 06:04:56 +0900

    Which Alien?(구현)

--- P6778.py
+++ P6778.py
@@ -3,7 +3,7 @@ def solution():
     input = sys.stdin.readline
     antenna = int(input())
     eye = int(input())
-    if antenna<=3 and 4<=eye: print("TroyMartian")
-    if antenna<=6 and 2<=eye: print("VladSaturnian")
-    if antenna<=2 and 3<=eye: print("GraemeMercurian")
+    if antenna>=3 and eye<=4: print("TroyMartian")
+    if antenna<=6 and eye>=2: print("VladSaturnian")
+    if antenna<=2 and eye<=3: print("GraemeMercurian")
 solution()
