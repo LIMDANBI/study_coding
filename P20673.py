391a8092fb76ebb95e8511e55ca6682fa8b95b64
danbibibi <dlaeksql08222@gmail.com>
2021-10-18 06:11:15 +0900

    Covid-19(구현)

---
+++ P20673.py
@@ -0,0 +1,9 @@
+def solution():
+    import sys
+    input = sys.stdin.readline
+    p = int(input())
+    q = int(input())
+    if(p<=50 and q<=10): print("White")
+    elif(30<=q): print("Red")
+    else: print("Yellow")
+solution()
