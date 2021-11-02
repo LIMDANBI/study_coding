22d7c2d5a8c93eebf019074dbbc983e3d022c25b
danbibibi <dlaeksql08222@gmail.com>
2021-11-02 16:12:57 +0900

    더하기 4(수학/사칙연산)

---
+++ P11024.py
@@ -0,0 +1,8 @@
+def solution():
+    import sys
+    input = sys.stdin.readline
+    T = int(input())
+    for i in range(T):
+        li = list(map(int, input().split()))
+        print(sum(li))
+solution()
