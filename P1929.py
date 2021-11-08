a1e7546b79bf0606ace194f3714bc9679b5ac5f6
danbibibi <dlaeksql08222@gmail.com>
2021-11-08 21:12:39 +0900

    소수 구하기(수학/정수론/소수판정/에라토스테네스의 체)

--- P1929.py
+++ P1929.py
@@ -1,13 +1,12 @@
-def isPrime(num):
-    if num==1: return False
-    for i in range(2, int(num**0.5)+1):
-        if num%i==0: return False
-    return True
-
 def solution():
     import sys
     input = sys.stdin.readline
     m, n = map(int, input().split())
-    for i in range(m, n+1):
-        if isPrime(i): print(i)
+    li = [False] + [True] * ((n - 1) // 2)
+    for x in range(1, int(n**0.5/2+1)):
+        if li[x]:
+            li[2 * x * (x + 1)::x * 2 + 1] = [False] * ((((n + 1) // 2) - x * x * 2) // (x * 2 + 1))
+    if m <= 2:
+        print(2)
+    print('\n'.join([f'{x}' for x, val in zip(range(m + (m & 1 == 0), n + 1, 2), li[m // 2:]) if val]))
 solution()
