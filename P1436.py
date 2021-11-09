f7526238b900bcef59ad7587c828d5d72a35c1e1
danbibibi <dlaeksql08222@gmail.com>
2021-11-10 02:50:05 +0900

    영화감독 숌(브루트포스 알고리즘)

--- P1436.py
+++ P1436.py
@@ -3,22 +3,19 @@ def solution():
     input = sys.stdin.readline
     n = int(input())
     nlist = []
-    mum = 0
+    num = 0
     while len(nlist) <= 10000:
-        if not mum % 10 == 6:
-            nlist.append(mum * 1000 + 666)
-
-        elif (mum // 10) % 100 == 66:
+        if not num % 10 == 6:
+            nlist.append(num * 1000 + 666)
+        elif (num // 10) % 100 == 66:
             for k in range(1000):
-                nlist.append(mum * 1000 + k)
-
-        elif (mum // 10) % 10 == 6:
+                nlist.append(num * 1000 + k)
+        elif (num // 10) % 10 == 6:
             for j in range(100):
-                nlist.append(mum * 1000 + 600 + j)
-
-        elif mum % 10 == 6:
+                nlist.append(num * 1000 + 600 + j)
+        elif num % 10 == 6:
             for i in range(10):
-                nlist.append(mum * 1000 + 660 + i)
-        mum += 1
+                nlist.append(num * 1000 + 660 + i)
+        num += 1
     print(nlist[n - 1])
 solution()
