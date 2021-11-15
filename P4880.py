9603dcefc3a0e6159ce3f48f573053385d313158
danbibibi <dlaeksql08222@gmail.com>
2021-11-15 15:21:48 +0900

    다음수 (수학/사칙연산)

---
+++ P4880.py
@@ -0,0 +1,9 @@
+def solution():
+    import sys
+    input = sys.stdin.readline
+    while(True):
+        a1, a2, a3 = map(int, input().split())
+        if a1==0 and a2==0 and a3==0: break
+        if a2-a1 == a3-a2: print("AP", a3+(a2-a1))
+        else: print("GP", a3*(a2//a1))
+solution()
