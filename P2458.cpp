6ec34e187610efaafe87ae87babe9f64a1d3c02f
danbibibi <dlaeksql08222@gmail.com>
2022-01-12 12:17:16 +0900

    키 순서(그래프 이론/그래프 탐색/플로이드-와샬)

--- P2458.cpp
+++ P2458.cpp
@@ -6,16 +6,6 @@ using namespace std;
 int N, M, a, b; 
 int cost[MAX_N][MAX_N]={};
 
-void floyd(){
-    for(int k=1; k<=N; k++){
-        for(int i=1; i<=N; i++){
-            for(int j=1; j<=N; j++){
-                cost[i][j] = min(cost[i][j], cost[i][k]+cost[k][j]);
-            }
-        }
-    }
-}
-
 int main(){
     cin>>N>>M;
     for(int i=1; i<=N; i++){
@@ -24,12 +14,18 @@ int main(){
     for(int i=0; i<M; i++){
         cin>>a>>b; cost[a][b]=1;
     }
-    floyd();
+    for(int k=1; k<=N; k++){ // floyd 알고리즘
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<=N; j++){
+                cost[i][j] = min(cost[i][j], cost[i][k]+cost[k][j]);
+            }
+        }
+    }
     int ans=0;
     for(int i=1; i<=N; i++){
         int cnt=0;
         for(int j=1; j<=N; j++){
-            if(cost[i][j]!=INF || cost[j][i]!=INF) cnt++;
+            if(cost[i][j]!=INF || cost[j][i]!=INF) cnt++; // 다른 정점과 연결되어 있는 경우
         }
         if(cnt==N-1) ans++;
     }
