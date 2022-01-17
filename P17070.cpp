c10fb539d6c1d4e7c10f2fce51d9375cbbbf1019
danbibibi <dlaeksql08222@gmail.com>
2022-01-17 13:02:20 +0900

    파이프 옮기기 1(다이나믹 프로그래밍/그래프 이론/그래프 탐색)

--- P17070.cpp
+++ P17070.cpp
@@ -1,3 +1,4 @@
+//[풀이 1] dfs를 이용한 풀이
 #include<iostream>
 #define MAX_N 17
 using namespace std;
@@ -35,4 +36,33 @@ int main(){
     }
     dfs(1, 2, 0); // (1, 2) , 가로
     cout << ans;
+}
+
+//[풀이 2] dp를 이용한 풀이
+#include<iostream>
+#include<cstring>
+#define MAX_N 17
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int home[MAX_N][MAX_N], dp[MAX_N][MAX_N][3];
+    memset(dp, 0, sizeof(dp)); // 초기화 
+    dp[1][2][0]=1; // 초기화 
+    
+    int N; cin>>N;
+    for(int i=1; i<=N; i++){
+        for(int j=1; j<=N; j++) cin >> home[i][j];
+    }
+    for(int i=1; i<=N; i++){ // dp (반복문을 돌면서 값을 하나씩 update해나감)
+        for(int j=3; j<=N; j++){
+            if(home[i][j]) continue;
+            dp[i][j][0] = dp[i][j-1][0]+dp[i][j-1][2]; // 가로
+            dp[i][j][1] = dp[i-1][j][1]+dp[i-1][j][2]; // 세로
+            if(!home[i-1][j] && !home[i][j-1]) // 대각선의 경우 추가 검사 필요
+                dp[i][j][2] = dp[i-1][j-1][0]+dp[i-1][j-1][1]+dp[i-1][j-1][2]; // 대각선
+            else dp[i][j][2]=0;
+        }
+    }
+    cout << dp[N][N][0] + dp[N][N][1] + dp[N][N][2]; // 가로, 세로, 대각선 도착 가짓수을 모두 더하면 총 도착 가짓수
 }
