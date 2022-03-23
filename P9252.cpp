8f12ea076c538e345b901552b1acf64526db56d9
danbibibi <dlaeksql08222@gmail.com>
2022-03-23 23:31:03 +0900

    LCS2(다이나믹 프로그래밍)

---
+++ P9252.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#define MAX 1001
+using namespace std;
+
+string s1, s2;
+int dp[MAX][MAX];
+
+void solution(){ 
+    int i, j;
+    for(i=0; i<s1.length(); i++){ // // DP를 통해 LCS 길이를 구함
+        for(j=0; j<s2.length(); j++){
+            if(s1[i] == s2[j]) dp[i+1][j+1] = dp[i][j]+1;
+            else dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j]);
+        }
+    } 
+    int lenLcs = dp[i][j];
+    
+    string lcs="";
+    while (lcs.length()!=lenLcs){ // LCS를 구함
+        if(dp[i][j] == dp[i-1][j-1]) { i--; j--;}
+        else if(dp[i][j] == dp[i-1][j]) i--;
+        else {
+            lcs+=s1[i];
+            if(dp[i][j] != dp[i-1][j-1]){ i--; j--;}
+            else i--;
+        }
+    }    
+    cout << lenLcs << '\n';
+    for(i = lcs.length()-1; i>=0; i--) cout << lcs[i];
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> s1 >> s2;
+    solution();
+}
