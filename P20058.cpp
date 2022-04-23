59559bddb7359e13e2cad36b50fddf0fa83047c1
danbibibi <dlaeksql08222@gmail.com>
2022-04-23 19:51:51 +0900

    마법사 상어와 파이어스톰(구현/그래프이론/그래프탐색/너비우선탐색/깊이우선탐색/시뮬레이션)

---
+++ P20058.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include<cstring>
+#include<queue>
+#define MAX 65
+using namespace std;
+
+int N, Q, L;
+int remain_ice=0,largest_mass=0;
+int A[MAX][MAX];
+bool visit[MAX][MAX], reduce[MAX][MAX];
+int dy[] = {-1, 1, 0, 0};
+int dx[] = {0, 0, -1, 1};
+int pow2[] = {1, 2, 4, 8, 16, 32, 64};
+
+void fireStorm(){
+    for(int i=1; i<=pow2[N]; i+=pow2[L]){ // 시계 방향 회전
+        for(int j=1; j<=pow2[N]; j+=pow2[L]){
+            queue<int> q;
+            int endR = i+pow2[L];
+            int endC = j+pow2[L];
+            for(int r=i; r<endR; r++){
+                for(int c=j; c<endC; c++) q.push(A[r][c]);
+            }
+            for(int c=endC-1; c>=j; c--){
+                for(int r=i; r<endR; r++){
+                    A[r][c] = q.front();
+                    q.pop();
+                }
+            }
+        }
+    }
+
+    memset(reduce, false, sizeof(reduce)); // 감소 여부 false로 초기화
+    for(int y=1; y<=pow2[N]; y++){ // 얼음양 감소
+        for(int x=1; x<=pow2[N]; x++){
+            int cnt=0;
+            for(int d=0; d<4; d++){
+                int ny = y+dy[d];
+                int nx = x+dx[d];
+                if(ny<1 || ny>pow2[N] || nx<1 || nx>pow2[N] || A[ny][nx]==0) continue; // 범위를 벗어나거나 얼음이 없는 경우
+                cnt++; 
+            }
+            if(cnt<3 && A[y][x]>0) reduce[y][x] = true; // 얼음이 있는 칸 3개 또는 그 이상과 인접해있지 않은 경우, 감소 여부 true
+        }
+    }
+    for(int y=1; y<=pow2[N]; y++){
+        for(int x=1; x<=pow2[N]; x++){
+            if(reduce[y][x]) A[y][x]--;
+        }
+    }
+}
+
+void bfs(int i, int j){ // largest_mass
+    int res = 1;
+    queue<pair<int, int>> q;
+    visit[i][j]=true;
+    q.push({i, j});
+    while (!q.empty()){
+        int y = q.front().first;
+        int x = q.front().second;
+        q.pop();
+        for(int d=0; d<4; d++){
+            int ny = y+dy[d];
+            int nx = x+dx[d];
+            if(ny<1 || ny>pow2[N] || nx<1 || nx>pow2[N] || visit[ny][nx] || A[ny][nx]<1) continue; // 범위를 벗어나는 경우, 이미 방문한 경우, 얼음이 없는 경우
+            visit[ny][nx] = true;
+            q.push({ny, nx});
+            res++;
+        }
+    }
+    largest_mass = max(largest_mass, res);
+}
+
+void ans(){
+    for(int i=1; i<=pow2[N]; i++){
+        for(int j=1; j<=pow2[N]; j++) {
+            remain_ice+=A[i][j]; // remain_ice
+            if(!visit[i][j] && A[i][j]) bfs(i, j); // largest_mass
+        }
+    }
+    cout << remain_ice << '\n' << largest_mass;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> N >> Q;
+    for(int i=1; i<=pow2[N]; i++){
+        for(int j=1; j<=pow2[N]; j++) cin >> A[i][j];
+    }
+    while(Q--){ // 파이어스톰을 Q번 시전
+        cin >> L;
+        fireStorm();
+    }
+    ans(); // remain_ice와 largest_mass를 구하고 출력
+}
