5689d493c7e855065f6da1bd9103737d99ef36be
danbibibi <dlaeksql08222@gmail.com>
2022-04-26 01:59:54 +0900

    상어 중학교(구현/그래프 이론/그래프 탐색/너비 우선 탐색/시뮬레이션/깊이 우선 탐색)

--- P21609.cpp
+++ P21609.cpp
@@ -51,7 +51,7 @@ pair<int, int> findGroup(int i, int j, int color){
             if(ny<1 || ny>N || nx<1 || nx>N) continue; // 범위를 벗어나는 경우
             if(map[ny][nx]==EMPTY || visit[ny][nx]) continue; // 비어 있거나, 이미 방문한 경우
             if(map[ny][nx]==BLACK || map[ny][nx]!=RAINBOW && map[ny][nx]!=color) continue; // black block이거나 다른 색인 경우 (무지개는 허용)
-            if(map[ny][nx]==RAINBOW) rbowSize++:
+            if(map[ny][nx]==RAINBOW) rbowSize++;
             visit[ny][nx] = true; // 방문
             groupSize++;
             q.push({ny, nx});
@@ -119,8 +119,9 @@ void autoPlay(){ // 블록 그룹이 존재하는 동안 반복
     for(int i=1; i<=N; i++){ // 블록 그룹 정보 저장
         for(int j=1; j<=N; j++){
             if(map[i][j]==BLACK || map[i][j]==RAINBOW || map[i][j]==EMPTY || visit[i][j]) continue;
-            int cnt = findGroup(i, j, map[i][j]); // BFS로 group check
-            if(cnt>=2) blockGroup.push({cnt, i, j}); // 기준 block을 pq에 저장 (그룹에 속한 블록의 개수는 2보다 크거나 같아야 함)
+            pair<int, int> res = findGroup(i, j, map[i][j]); // BFS로 group check
+            int cnt=res.first, rbow=res.second;
+            if(cnt>=2) blockGroup.push({cnt, rbow, i, j}); // 기준 block을 pq에 저장 (그룹에 속한 블록의 개수는 2보다 크거나 같아야 함)
         }
     }
     if(blockGroup.size()==0){
