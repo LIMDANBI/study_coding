c7c4684ad2be0fbdb0fa8f413f6483aae886f8d4
danbibibi <dlaeksql08222@gmail.com>
2022-04-28 19:27:27 +0900

    낚시왕(구현/시뮬레이션)

--- P17143.cpp
+++ P17143.cpp
@@ -1,67 +1,91 @@
 #include<iostream>
+#include<cstring>
 #define MAX 101
+#define EMPTY 0
 using namespace std;
 
-struct shark { int speed, direction, size; };
+struct SHARK {
+	int r, c, s, d, z;  // 위치, 속력, 이동방향, 크기
+	bool isdead; // 죽은 상어인지
+};
+SHARK shark[MAX * MAX];
 
-int R, C, M; // 격자판의 크기, 상어의 수
-int ans = 0; // 낚시왕이 잡은 상어 크기의 합
-shark grid[MAX][MAX], copy_grid[MAX][MAX];
+int ans = 0;
+int R, C, M; //격자판의 크기, 상어의 수
+int fish_manR = 0, fish_manC = 0; // 낚시왕 위치
+int map[MAX][MAX], tmpMap[MAX][MAX]; // 상어가 있는지
+int dr[] = { -1, 1, 0, 0 }; // 위 아래 오른쪽 왼쪽 (1~4 -> 0~3)
+int dc[] = { 0, 0, 1, -1 };
 
-int dr[] = {0, -1, 1, 0, 0}; // d가 1인 경우는 위, 2인 경우는 아래, 3인 경우는 오른쪽, 4인 경우는 왼쪽
-int dc[] = {0, 0, 0, 1, -1};
-
-void move_shark(){
-    for(int r=1; r<=R; r++){
-        for(int c=1; c<=C; c++){
-            if(grid[r][c].size==0) continue; // 상어가 없는 경우
-            int nr=r, nc=c;
-            int move_cnt = grid[r][c].speed;
-            int move_dir = grid[r][c].direction;
-            if(move_dir<3) move_cnt%=(2*R-2); // 시간초과 방지
-            else move_cnt%=(2*C-2);
-            while (move_cnt--){ // 상어가 있는 경우, 상어 이동
-                nr+=dr[move_dir]; 
-                nc+=dc[move_dir];
-                if(nr<1 || nr>R || nc<1 || nc>C){ // 격자를 벗어나는 경우
-                    if(move_dir==1 || move_dir==3) move_dir++; // 방향을 바꿈
-                    else move_dir--; // 방향을 바꿈
-                    nr+=dr[move_dir]*2; // 바꾼 방향으로 제대로 이동 
-                    nc+=dc[move_dir]*2;
-                }
-            }
-            grid[r][c].direction = move_dir; // 바뀐 방향 저장 !!!!!!!
-            if(copy_grid[nr][nc].size==0) // 이동을 마친 곳에 다른 상어가 없는 경우
-                copy_grid[nr][nc] = grid[r][c];
-            else if(copy_grid[nr][nc].size < grid[r][c].size) // 다른 상어가 있는 경우, 큰 상어가 나머지 상어를 잡아 먹음
-                copy_grid[nr][nc] = grid[r][c];
-        }
-    }
-    for(int r=1; r<=R; r++){ 
-        for(int c=1; c<=C; c++){
-            grid[r][c] = copy_grid[r][c]; // copy_gird -> gird 복제
-            copy_grid[r][c] = {0,0,0}; // 초기화
-        }
-    }
+void input() {
+	cin >> R >> C >> M;
+	int r, c, s, d, z; // 위치, 속력, 이동방향, 크기
+	for (int i = 1; i <= M; i++) {
+		cin >> r >> c >> s >> d >> z;
+		map[r][c] = i; // 상어 여부 저장 (EMPTY(0) 아닌 경우 상어 존재)
+		shark[i] = { r, c, s, d - 1, z, false }; // 상어 정보 저장
+	}
 }
 
-int main(){
-    ios_base::sync_with_stdio(0); cin.tie(0);
-    cin >> R >> C >> M;
-    int r, c, s, d, z; // 상어의 정보 (위치, 속력, 이동 방향, 크기)
-    while (M--){
-        cin >> r >> c >> s >> d >> z;
-        grid[r][c] = {s, d, z};
-    }
-    for(int i=1; i<=C; i++){ // 1. 낚시왕이 오른쪽으로 한 칸씩 이동
-        for(int j=1; j<=R; j++){
-            if(grid[j][i].size==0) continue; // 상어가 없는 경우
-            ans+=grid[j][i].size; // 2. 낚시왕이 땅과 제일 가까운 상어를 잡음
-            // cout << i << ' ' << grid[j][i].size << '\n';
-            grid[j][i] = {0,0,0}; // 상어를 잡았으므로 비워줌
-            break;
-        }
-        move_shark(); // 3. 상어 이동
-    }
-    cout << ans;
+void catchShark() {
+	fish_manC++; // 낚시왕이 오른쪽으로 한 칸 이동
+	for (int r = 1; r <= R; r++) {
+		if (map[r][fish_manC] == EMPTY) continue;
+		int sharkNum = map[r][fish_manC];
+		int y = shark[sharkNum].r;
+		int x = shark[sharkNum].c;
+		// 상어를 잡음
+		ans += shark[sharkNum].z;
+		map[y][x] = EMPTY;
+		shark[sharkNum].isdead = true;
+		return;
+	}
+}
+
+void moveShark() {
+	memset(tmpMap, EMPTY, sizeof(tmpMap)); // 초기화
+	for (int i = 1; i <= M; i++) { // i 번째로 들어온 상어
+		if (shark[i].isdead) continue; // 이미 죽은 상어인 경우 이동 x
+		int s = shark[i].s; // 상어의 속력 (칸/초)
+		int r = shark[i].r;
+		int c = shark[i].c;
+		int d = shark[i].d;
+		if (d < 2) s %= (2 * R - 2);
+		else s %= (2 * C - 2);
+		while (s--) { // s칸 이동
+			r += dr[d];
+			c += dc[d];
+			if (r<1 || r>R || c<1 || c>C) { // 범위를 벗어나는 경우
+				if (d == 0 || d == 2) d++; // 이동 방향을 반대로 바꿈
+				else d--;
+				shark[i].d = d; // 바뀐 방향 저장
+				r += dr[d] * 2; // 잘못 갔던 것까지 돌려 놓기 위해 *2
+				c += dc[d] * 2;
+			}
+		}
+		// 상어 위치 정보 수정
+		shark[i].r = r;
+		shark[i].c = c;
+		if (tmpMap[r][c] == EMPTY) tmpMap[r][c] = i; // 비어 있는 경우
+		else { // 다른 상어가 있는 경우
+			int before = tmpMap[r][c];
+			if (shark[before].z > shark[i].z) shark[i].isdead = true;
+			else {
+				shark[before].isdead = true;
+				tmpMap[r][c] = i;
+			}
+		}
+	}
+	for (int i = 1; i <= R; i++) { // 배열 복사
+		for (int j = 1; j <= C; j++) map[i][j] = tmpMap[i][j];
+	}
+}
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	input();
+	while (fish_manC < C) {
+		catchShark(); // 상어 잡음
+		moveShark(); // 상어 이동
+	}
+	cout << ans;
 }
