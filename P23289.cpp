cfc08764675681a0d83dde5bd15d192d5afa8e78
danbibibi <dlaeksql08222@gmail.com>
2022-04-30 02:14:46 +0900

    온풍기 안녕! (구현/시뮬레이션)

--- P23289.cpp
+++ P23289.cpp
@@ -11,21 +11,22 @@
 #define CHECK 5 //5 : 온도를 조사해야 하는 칸
 using namespace std;
 
-struct TEMP { int r, c, d, k; };
+struct TEMP { int r, c, k; };
 struct HEATER { int r, c, d; };
 vector<HEATER> heater;
 vector<pair<int, int>> checkPos;
 
+int chocolate = 0;
 int R, C, K, W; // RxC, 온도가 K 이상이 되었는지, 벽의 개수
 int map[MAX][MAX];
 bool wall[MAX][MAX][5], visit[MAX][MAX];
 int temperature[MAX][MAX], tmpTemperature[MAX][MAX];
 int dr[] = { 0, 0, 0, -1, 1 }; // 오른쪽, 왼쪽, 위, 아래
 int dc[] = { 0, 1, -1, 0, 0 };
-int dh[5][2][3] = { // dh[1][1][0] 오른쪽을 바라볼 때 c 좌표 0번째
+int dh[5][2][3] = { // dh[1][0][0] 오른쪽(1, RIGHT)을 바라볼 때 r 좌표 0번째
 	{{ 0, 0, 0 }, { 0, 0, 0 }},
 	{{ -1, 0, 1 }, { 1, 1, 1 }}, // 오른쪽
-	{{ 1, 0, -1 }, { -1, -1, -1 }}, // 왼쪽
+	{{ -1, 0, 1 }, { -1, -1, -1 }}, // 왼쪽
 	{{ -1, -1, -1 }, { -1, 0, 1}}, // 위
 	{{ 1, 1, 1 }, { 1, 0, -1 }}, // 아래
 };
@@ -35,13 +36,13 @@ void input() {
 	for (int i = 1; i <= R; i++) { //  집 상태 정보
 		for (int j = 1; j <= C; j++) {
 			cin >> map[i][j];
-			if (0 < map[i][j] && map[i][j] < 5) heater.push_back({ i, j, map[i][j] });
+			if (RIGHT <= map[i][j] && map[i][j] <= DOWN) heater.push_back({ i, j, map[i][j] });
 			if (map[i][j] == CHECK) checkPos.push_back({ i,j });
 		}
 	}
 	cin >> W;
 	int x, y, t;
-	for (int i = 1; i <= W; i++) { // 벽 정보 -  t = 0, (x, y)와 (x-1, y) 사이 벽 / t = 1, (x, y)와 (x, y+1) 사이 벽
+	for (int i = 0; i < W; i++) { // 벽 정보 -  t = 0, (x, y)와 (x-1, y) 사이 벽 / t = 1, (x, y)와 (x, y+1) 사이 벽
 		cin >> x >> y >> t;
 		if (t == 0) {
 			wall[x][y][UP] = true; // (x,y) 입장에서는 위(3)에 벽이 생기는 것
@@ -59,17 +60,16 @@ void bfs(int h) {
 	int r = heater[h].r;
 	int c = heater[h].c;
 	int d = heater[h].d; // 온풍기 방향
-	r += dr[d]; 
+	r += dr[d];
 	c += dc[d];
-	q.push({ r,c,d,5 });
+	q.push({ r,c,5 });
 	visit[r][c] = true;
 	while (!q.empty()) {
 		int r = q.front().r;
 		int c = q.front().c;
-		int d = q.front().d;
 		int k = q.front().k;
 		q.pop();
-		
+
 		tmpTemperature[r][c] = k;
 		for (int i = 0; i < 3; i++) {
 			int nr = r + dh[d][0][i];
@@ -87,7 +87,7 @@ void bfs(int h) {
 				}
 			}
 			visit[nr][nc] = true;
-			if (k - 1 > 0) q.push({ nr, nc, d, k - 1 });
+			if (k - 1 > 0) q.push({ nr, nc, k - 1 });
 		}
 	}
 }
@@ -105,9 +105,8 @@ void wind() {
 
 void control() { // 온도 조절
 	memset(tmpTemperature, 0, sizeof(tmpTemperature)); // 초기화
-	for (int i = 1; i <= R; i++) { 
+	for (int i = 1; i <= R; i++) {
 		for (int j = 1; j <= C; j++) {
-			int temp = temperature[i][j];
 			for (int d = 1; d < 5; d++) {
 				int nr = i + dr[d];
 				int nc = j + dc[d];
@@ -152,7 +151,6 @@ bool check() { // 조사하는 모든 칸의 온도가 K 이상이 되었는지 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 	input();
-	int chocolate = 0;
 	while (chocolate < 101) {
 		wind();
 		control();
