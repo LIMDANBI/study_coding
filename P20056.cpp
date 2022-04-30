d0672d59901d85f84002b8f2753b9d449dabeb08
danbibibi <dlaeksql08222@gmail.com>
2022-05-01 00:35:00 +0900

    마법사 상어와 파이어볼(구현/시뮬레이션)

--- P20056.cpp
+++ P20056.cpp
@@ -1,81 +1,80 @@
 #include<iostream>
 #include<vector>
-#define MAX 51
+#define MAX 50 
 using namespace std;
 
-struct FIREBALL{int m, s, d;};
-vector<FIREBALL> map[MAX][MAX];
+struct MSD { int m, s, d; };
+vector<MSD> map[MAX][MAX], tmpMap[MAX][MAX];
 
-int N, M, K, ans=0;
-int dy[] = {-1, -1, 0, 1, 1, 1, 0, -1}; // ↑, ↗, →, ↘, ↓, ↙, ←, ↖,
-int dx[] = {0, 1, 1, 1, 0, -1, -1, -1};
+int N, M, K;
+int dr[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int dc[] = {0, 1, 1, 1, 0, -1, -1, -1};
 
-void moveFireball(){
-    vector<FIREBALL> tmpMap[MAX][MAX];
-    for(int i=1; i<=N; i++){ // 모든 파이어볼이 자신의 방향 d로 속력 s칸 만큼 이동
-        for(int j=1; j<=N; j++){
-            for(int k=0; k<map[i][j].size(); k++){
-                int m = map[i][j][k].m; // 질량
-                int s = map[i][j][k].s; // 속력
-                int move = s%N;
-                int d = map[i][j][k].d; // 방향
-                int ny = i+dy[d]*move;
-                int nx = j+dx[d]*move;
-                if(ny<1) ny+=N; if(ny>N) ny-=N; // 1번 행은 N번 행과 연결 
-                if(nx<1) nx+=N; if(nx>N) nx-=N; // 1번 열은 N번 열과 연결
-                tmpMap[ny][nx].push_back({m, s, d});
-            }
-        }
-    }
-    for(int i=1; i<=N; i++){ // 이동 상태 복사
-        for(int j=1; j<=N; j++) map[i][j] = tmpMap[i][j];
-    }
+void input() {
+	cin >> N >> M >> K; // 격자 크기, 파이어볼 개수, 이동명령 횟수
+	int  r, c, m, s, d; // 위치, 질량, 속력, 방향
+	for (int i = 0; i < M; i++) {
+		cin >> r >> c >> m >> s >> d;
+		map[r - 1][c - 1].push_back({ m,s,d });
+	}
 }
 
-void afterMove(){
-    for(int i=1; i<=N; i++){
-        for(int j=1; j<=N; j++){
-            if(map[i][j].size()>=2){ // 2개 이상의 파이어볼이 있는 칸
-                int mass=0, speed=0;
-                int dir = map[i][j][0].d; // 이전 방향
-                bool isAll = true;
-                for(int k=0; k<map[i][j].size(); k++){
-                    mass+=map[i][j][k].m;
-                    speed+=map[i][j][k].s;
-                    int now = map[i][j][k].d; 
-                    if(dir%2!=now%2) isAll=false;
-                }
-                mass/=5; speed/=map[i][j].size();
-                map[i][j].clear(); // 해당 칸을 비우고 
-                if(isAll && mass!=0){ // 파이어볼의 방향이 모두 홀수이거나 모두 짝수 (질량이 0인 파이어볼은 소멸되어 없어짐)
-                    for(int d=0; d<4; d++)
-                        map[i][j].push_back({mass, speed, d*2}); // 방향은 0, 2, 4, 6
-                }
-                else if(mass!=0){
-                    for(int d=0; d<4; d++)
-                        map[i][j].push_back({mass, speed, d*2+1}); // 방향은 1, 3, 5, 7
-                }
-            }
-        }
-    }
+void moveorder() { // 마법사 상어의 이동 명령
+	for (int i = 0; i < N; i++) { // 파이어볼 이동
+		for (int j = 0; j < N; j++) {
+			for (int k = 0; k < map[i][j].size(); k++) {
+				int m = map[i][j][k].m;
+				int s = map[i][j][k].s;
+				int d = map[i][j][k].d;
+				int nr = i + dr[d] * s;
+				int nc = j + dc[d] * s;
+				nr %= N; if (nr < 0) nr += N; // 1번 행은 N번과 연결
+				nc %= N; if (nc < 0) nc += N; // 1번 열은 N번 열과 연결
+				tmpMap[nr][nc].push_back({ m,s,d });
+			}
+			map[i][j].clear();
+		}
+	}
+	for (int i = 0; i < N; i++) { 
+		for (int j = 0; j < N; j++) {
+			if (tmpMap[i][j].size() > 1) { // 2개 이상의 파이어볼이 있는 칸
+				int nm = 0, ns = 0, nd; // 새로운 질량, 속력, 방향
+				int before = tmpMap[i][j][0].d;
+				int flag = true;
+				for (int k = 0; k < tmpMap[i][j].size(); k++) {
+					nm += tmpMap[i][j][k].m;
+					ns += tmpMap[i][j][k].s;
+					nd = tmpMap[i][j][k].d;
+					if (before % 2 != nd % 2) flag = false;
+				}
+				nm /= 5;
+				ns /= (int)tmpMap[i][j].size();
+				int start_dir;
+				flag ? start_dir = 0 : start_dir = 1; // 방향이 모두 홀수이거나 모두 짝수이면 0, 2, 4, 6 / 그렇지 않으면 1, 3, 5, 7
+				if (nm != 0) { // 질량이 0인 파이어볼은 소멸되어 없어짐
+					for (int d = start_dir; d < 8; d += 2)
+						map[i][j].push_back({ nm,ns,d });
+				}
+			}
+			else map[i][j] = tmpMap[i][j]; // 파이어볼이 1개 있는 칸
+			tmpMap[i][j].clear();
+		}
+	}
 }
 
-int main(){
-    ios_base::sync_with_stdio(0); cin.tie(0);
-    cin >> N >> M >> K;
-    int r, c, m, s, d; // 위치, 질량, 속력, 방향
-    for(int i=0; i<M; i++){
-        cin >> r >> c >> m >> s >> d;
-        map[r][c].push_back({m, s, d});
-    }
-    while(K--){ // K번 이동 명령
-        moveFireball(); // 파이어볼 이동
-        afterMove(); // 파이어볼 이동 후
-    }
-    for(int i=1; i<=N; i++){
-        for(int j=1; j<=N; j++){
-            for(int k=0; k<map[i][j].size(); k++) ans+=map[i][j][k].m;
-        }
-    }
-    cout << ans;
+int output() {  // 남아있는 파이어볼 질량의 합
+	int ans = 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			for (int k = 0; k < map[i][j].size(); k++) ans += map[i][j][k].m;
+		}
+	}
+	return ans;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	input();
+	while (K--) moveorder(); // 마법사 상어가 이동을 K번 명령
+	cout << output(); // 남아있는 파이어볼 질량의 합
 }
