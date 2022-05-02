0a6b7dc02f284999b65756154652735921c9d50e
danbibibi <dlaeksql08222@gmail.com>
2022-05-02 23:41:35 +0900

    숫자 문자열과 영단어

---
+++ 2021_Kakao_Internship/P1.py
@@ -0,0 +1,15 @@
+def solution(s):
+    li_word = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    answer = ""
+    tmp = ""
+    for i in s:
+        if "0"<= i and i<="9":
+            answer+=i
+        else:
+            tmp+=i
+            if tmp in li_word:
+                idx = li_word.index(tmp)
+                answer+=str(idx+1)
+                tmp = ""
+    answer = int(answer)
+    return answer
