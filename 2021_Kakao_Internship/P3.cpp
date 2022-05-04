32e1b1ea0a8eb19808b79b73885543e784743e46
danbibibi <dlaeksql08222@gmail.com>
2022-05-05 01:40:01 +0900

    표 편집

--- 2021_Kakao_Internship/P3.cpp
+++ 2021_Kakao_Internship/P3.cpp
@@ -3,60 +3,93 @@
 #include <stack>
 using namespace std;
 
-struct DATA{
-    int num;
-    bool isdelecte;
+struct Node{
+    int data;
+    struct Node *prev;
+    struct Node *next;
 };
 
-vector<DATA> v;
-stack<int> s;
-int cur, last;
+stack<Node*> s;
+Node *header, *cur;
+
+void init(int n, int k){
+    header = new Node();
+    header->data = 0;
+    header->prev = NULL;
+    header->next = NULL;
+    cur = header;
+    for(int i=1; i<n; i++){
+        Node *n = new Node();
+        n->data = i;
+        cur->next = n;
+        n->prev = cur;
+        n->next = NULL;
+        cur = n;
+    }
+    cur = header;
+    for(int i=0; i<k; i++) cur = cur->next;
+}
 
 void upSelect(int x){ // 현재 선택된 행에서 X칸 위에 있는 행을 선택
-    while (x){
-        cur--;
-        if(!v[cur].isdelecte) x--;
-    }
+    while (x--) cur = cur->prev;
 }
 
 void downSelect(int x){ // 현재 선택된 행에서 X칸 아래에 있는 행을 선택
-    while (x){
-        cur++;
-        if(!v[cur].isdelecte) x--;
-    }
+    while (x--) cur = cur->next;
 }
 
 void delecte(){ // 현재 선택된 행을 삭제한 후, 바로 아래 행을 선택 (삭제된 행이 가장 마지막 행인 경우 바로 윗 행을 선택)
-    v[cur].isdelecte = true;
     s.push(cur);
-    if(cur == last){
-        while (!v[cur].isdelecte) cur--;
+    if(cur->prev==NULL){
+        cur = cur->next;
+        cur->prev = NULL;
+        header = cur;
+    }
+    else if(cur->next==NULL){
+        cur = cur->prev;
+        cur->next = NULL;
     }
     else{
-        while (!v[cur].isdelecte) cur++;
+        cur->prev->next = cur->next;
+        cur->next->prev = cur->prev;
+        cur = cur->next;
     }
-    last = cur;
 }
 
 void restore(){ // 가장 최근에 삭제된 행을 원래대로 복구 (현재 선택된 행은 바뀌지 x)
-    int row = s.top(); s.pop();
-    v[row].isdelecte = false;
-    if(row>last) last = row;
+    Node *n = s.top(); s.pop();
+    if(n->prev==NULL){
+        n->next->prev = n;
+        header = n;
+    }
+    else if(n->next==NULL){
+        n->prev->next = n;
+    }
+    else{
+        n->prev->next = n;
+        n->next->prev = n;
+    }
+}
+
+void solution(vector<string> cmd){
+    for(int i=0; i<cmd.size(); i++){
+        if(cmd[i][0]=='U') upSelect(stoi(cmd[i].substr(2)));
+        else if(cmd[i][0]=='D') downSelect(stoi(cmd[i].substr(2)));
+        else if(cmd[i][0]=='C') delecte();
+        else if(cmd[i][0]=='Z') restore();
+    }
 }
 
 string solution(int n, int k, vector<string> cmd) {
     string answer = "";
-    cur = k; last=n-1;
-    for(int i=0; i<n; i++) v.push_back({i, false});
-    for(int c=0; c<cmd.size(); c++){
-        if(cmd[c][0]=='U') upSelect(stoi(cmd[c].substr(2, cmd[c].length()-1)));
-        else if(cmd[c][0]=='D') downSelect(stoi(cmd[c].substr(2, cmd[c].length()-1)));
-        else if(cmd[c][0]=='C') delecte();
-        else if(cmd[c][0]=='Z') restore();
-    }
-    for(int i=0; i<n; i++){
-        if(v[i].isdelecte) answer+="X";
-        else answer+="O";
+    for(int i=0; i<n; i++) answer+="X";
+    init(n, k);
+    solution(cmd);
+    cur = header;
+    while (1){
+        answer[cur->data] = 'O';
+        cur = cur->next;
+        if(cur == NULL) break;
     }
     return answer;
 }
