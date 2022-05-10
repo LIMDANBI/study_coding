681c6a734bfb556c0959f96f0c2c9ea88fd73e59
danbibibi <dlaeksql08222@gmail.com>
2022-05-11 00:01:37 +0900

    알파벳(그래프이론/그래프탐색/깊이우선탐색/백트래킹)

--- P1987.cpp
+++ P1987.cpp
@@ -2,25 +2,23 @@
 #define MAX 20
 using namespace std;
 
-int R, C, ans=1;
+int R, C, ans=0;
 bool visit[26];
 int map[MAX][MAX];
 int dr[] = {-1, 1, 0, 0};
 int dc[] = {0, 0, -1, 1};
 
 void backtracking(int r, int c, int cnt){
-    if(visit[map[r][c]]){
-        ans = max(ans, cnt-1);
-        return;
-    }
-    visit[map[r][c]] = true;
+    ans = max(ans, cnt);
     for(int d=0; d<4; d++){
         int nr = r + dr[d];
         int nc = c + dc[d];
         if(nr<0 || nr>=R || nc<0 || nc>=C) continue; // 범위를 벗어나는 경우
+        if(visit[map[nr][nc]]) continue; // 이미 방문한 경우
+        visit[map[nr][nc]] = true;
         backtracking(nr, nc, cnt+1);
+        visit[map[nr][nc]] = false;
     }
-    visit[map[r][c]] = false;
 }
 
 int main(){
@@ -31,6 +29,7 @@ int main(){
         cin >> str;
         for(int j=0; j<C; j++) map[i][j] = str[j]-'A';
     }
+    visit[map[0][0]] = true;
     backtracking(0, 0, 1); // 좌측 상단에서 시작 (좌측 상단 칸도 포함)
     cout << ans;
 }
