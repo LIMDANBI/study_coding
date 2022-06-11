b1c7a47781d83808124a9484d91a247c5ee53033
danbibibi <dlaeksql08222@gmail.com>
2022-06-11 19:14:17 +0900

    N-Queen(브루트포스 알고리즘/백트래킹)

--- P9663.cpp
+++ P9663.cpp
@@ -1,32 +1,30 @@
-#include<iostream>
-#define MAX_N 15
+#include <iostream>
+#define MAX 15
 using namespace std;
 
-int N, ans, board[MAX_N];
+int N, ans, board[MAX];
 
-int promising(int r){ // 유망한지 판단
-    for(int i=0; i<r; i++){
-        if(board[r]==board[i] || r-i == abs(board[r]-board[i])) // 같은 열, 대각선에 있으는 경우
-            return 0;
+bool promising(int r){
+    for(int i=1; i<r; i++){
+        if(board[r]==board[i] || r-i == abs(board[r]-board[i])) return false; // 같은 열, 대각선에 있는 경우
     }
-    return 1;
+    return true;
 }
 
 void backtracking(int r){
-    if(r==N){
-        ans++; 
-        return;
+    if(r==N+1){
+        ans++;
+        return ;
     }
-    for(int c=0; c<N; c++){
+    for(int c=1; c<=N; c++){
         board[r] = c; // r열 c행에 queen을 놓아봄
-        if(promising(r)) // 유망한 경우
-            backtracking(r+1); // 다음 행에 퀸을 놓아봄
+        if(promising(r)) backtracking(r+1); // 유망한 경우 다음 행에 퀸을 놓음
     }
 }
 
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
-    cin>>N;
-    backtracking(0);
+    cin >> N;
+    backtracking(1);
     cout << ans;
 }
