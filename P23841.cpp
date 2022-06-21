482234f5aa0c1875499711d70430f017d56d2659
danbibibi <dlaeksql08222@gmail.com>
2022-06-21 23:29:05 +0900

    데칼코마니(구현/문자열)

--- P23841.cpp
+++ P23841.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
-    char map[MAX][MAX], c;
+    char map[MAX][MAX];
     int N, M; cin >> N >> M;
     for(int i=0; i<N; i++){
+        string s; cin >> s;
+        for(int j=0; j<M; j++) map[i][j] = map[j];
+    }
+    for(int i=0; i<N; i++){
         for(int j=0; j<M; j++){
-            cin >> c;
-            if('A'<=map[i][j] && map[i][j]<='Z') continue;
-            map[i][j] = c;
-            if(c=='.') continue;
-            if(j<M/2) map[i][M-j-1] = map[i][j];
-            else map[i][j] = map[i][M-j-1];
+            if(map[i][j]=='.') continue;
+            map[i][M-j-1] = map[i][j];
         }
     }
     for(int i=0; i<N; i++){
