f35009ba21d1437624b5bfa24d32f0037c578b06
danbibibi <dlaeksql08222@gmail.com>
2022-07-09 17:12:51 +0900

    센서(그리디 알고리즘/정렬)

--- P2212.cpp
+++ P2212.cpp
@@ -1,14 +1,22 @@
 #include<iostream>
 #include<algorithm>
+#include<vector>
 #define MAX 10001
 using namespace std;
 
-int N, K, map[MAX];
-
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
+    int N, K, tmp, ans=0;
     cin >> N >> K;
-    for(int i=0; i<N; i++) cin >> map[i];
-    sort(map, map+N);
-    
+    vector<int> sensor;
+    for(int i=0; i<N; i++){
+        cin >> tmp;
+        sensor.push_back(tmp);
+    }
+    sort(sensor.begin(), sensor.end());
+    vector<int> dist;
+    for(int i=1; i<N; i++) dist.push_back(sensor[i]-sensor[i-1]);
+    sort(dist.begin(), dist.end());
+    for(int i=0; i<N-K; i++) ans+=dist[i];
+    cout << ans;
 }
