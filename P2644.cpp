30c60ea504d70cbc5ab44a44ad38a455601635ca
danbibibi <dlaeksql08222@gmail.com>
2022-07-11 19:27:02 +0900

    촌수계산(그래프 이론/그래프 탐색/너비 우선 탐색/깊이 우선 탐색)

--- P2644.cpp
+++ P2644.cpp
@@ -4,7 +4,22 @@
 using namespace std;
 
 int n, m, p1, p2;
+bool isfind = false;
+bool visit[MAX];
 vector<int> v[MAX];
+
+void dfs(int node, int cnt){
+    visit[node] = true;
+    if(node == p2){
+        cout << cnt;
+        isfind = true;
+        return;
+    }
+    for(int i=0; i<v[node].size(); i++){
+        if(visit[v[node][i]]) continue;
+        dfs(v[node][i], cnt+1);
+    }
+}
 
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
@@ -15,7 +30,6 @@ int main(){
         v[x].push_back(y);
         v[y].push_back(x);
     }
-    int ans = -1;
-
-    cout << ans;
+    for(int i=0; i<v[p1].size(); i++) dfs(v[p1][i], 1);
+    if(!isfind) cout << -1;
 }
