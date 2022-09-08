5b766e129f19249c9e708ae973f7c6d67b933a10
LIMDANBI <dlaeksql08222@gmail.com>
2022-09-08 21:47:43 +0900

    가장 긴 감소하는 부분 수열(다이나믹 프로그래밍)

---
+++ P11722.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#define MAX 1001
+using namespace std;
+
+int N, ans=0;
+int A[MAX], dp[MAX];
+
+void input(){
+    cin >> N;
+    for(int i=1; i<=N; i++) cin >> A[i];
+}
+
+void solution(){
+    for(int i=1; i<=N; i++){
+        dp[i] = 1;
+        for(int j=1; j<=i; j++){
+            if(A[i]<A[j]) dp[i] = max(dp[j]+1, dp[i]);
+        }
+        ans = max(ans, dp[i]);
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    input();
+    solution();
+    cout << ans;
+}
