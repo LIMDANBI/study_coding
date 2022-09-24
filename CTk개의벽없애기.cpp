10e7c32718c0ce4b9383e4fdd58c711bada7540d
LIMDANBI <dlaeksql08222@gmail.com>
2022-09-25 03:28:27 +0900

    k개의 벽 없애기(BFS/Backtracking) - 시간 단축

--- CTk개의벽없애기.cpp
+++ CTk개의벽없애기.cpp
@@ -11,6 +11,7 @@ int r1, c1, r2, c2;
 int map[MAX][MAX];
 int dist[MAX][MAX];
 bool visited[MAX][MAX];
+vector<pair<int, int>> pos;
 
 int dy[] = {-1,1,0,0};
 int dx[] = {0,0,-1,1};
@@ -18,7 +19,10 @@ int dx[] = {0,0,-1,1};
 void input(){
     cin >> n >> k;
     for(int i=1; i<=n; i++){
-        for(int j=1; j<=n; j++) cin >> map[i][j];
+        for(int j=1; j<=n; j++){
+            cin >> map[i][j];
+            if(map[i][j]) pos.push_back({i,j}); // 벽의 위치 저장
+        }
     }
     cin >> r1 >> c1 >> r2 >> c2;
 }
@@ -48,26 +52,22 @@ int bfs(){
     return INF;
 }
 
-void solution(int cnt){
+void solution(int idx, int cnt){
     if(cnt==k){
         ans = min(ans, bfs());
         return;
     }
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=n; j++){
-            if(map[i][j]){
-                map[i][j] = 0;
-                solution(cnt+1);
-                map[i][j] = 1;
-            }
-        }
-    }
+    int y = pos[idx].first;
+    int x = pos[idx].second;
+    map[y][x] = 0;
+    solution(idx+1, cnt+1);
+    map[y][x] = 1;
 }
 
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
     input();
-    solution(0);
+    solution(0, 0);
     if(ans==INF) cout << -1;
     else cout << ans;
 }
