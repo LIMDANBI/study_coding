7a71f25d3b0b3fa29207eb26bf2780955b61c514
LIMDANBI <dlaeksql08222@gmail.com>
2022-09-25 03:20:41 +0900

    k개의 벽 없애기(BFS/Backtracking)

---
+++ CTk개의벽없애기.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<cstring>
+#include<vector>
+#include<queue>
+#define MAX 101
+#define INF 987654321
+using namespace std;
+
+int n, k, ans=INF;
+int r1, c1, r2, c2;
+int map[MAX][MAX];
+int dist[MAX][MAX];
+bool visited[MAX][MAX];
+
+int dy[] = {-1,1,0,0};
+int dx[] = {0,0,-1,1};
+
+void input(){
+    cin >> n >> k;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++) cin >> map[i][j];
+    }
+    cin >> r1 >> c1 >> r2 >> c2;
+}
+
+int bfs(){
+    memset(visited, false, sizeof(visited));
+    memset(dist, 0, sizeof(dist));
+    queue<pair<int,int>> q;
+    visited[r1][c1] = true;
+    q.push({r1, c1});
+
+    while(!q.empty()){
+        int y = q.front().first;
+        int x = q.front().second;
+        q.pop();
+        for(int d=0; d<4; d++){
+            int ny = y+dy[d];
+            int nx = x+dx[d];
+            if(ny<1 || ny>n || nx<1 || nx>n) continue;
+            if(visited[ny][nx] || map[ny][nx]) continue;
+            q.push({ny,nx});
+            visited[ny][nx] = true;
+            dist[ny][nx] = dist[y][x]+1;
+            if(ny==r2 && nx==c2) return dist[ny][nx];
+        }
+    }
+    return INF;
+}
+
+void solution(int cnt){
+    if(cnt==k){
+        ans = min(ans, bfs());
+        return;
+    }
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            if(map[i][j]){
+                map[i][j] = 0;
+                solution(cnt+1);
+                map[i][j] = 1;
+            }
+        }
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    input();
+    solution(0);
+    if(ans==INF) cout << -1;
+    else cout << ans;
+}
