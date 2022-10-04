12edfcaf61c11cfb9a379b35357c5f5fe41d8e95
LIMDANBI <dlaeksql08222@gmail.com>
2022-10-04 10:08:03 +0900

    피보나치 수의 확장(수학/다이나믹 프로그래밍)

---
+++ P1788.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#define MOD 1000000000
+#define MAX 1000001
+using namespace std;
+
+int n, sign=1, dp[MAX];
+
+void solution(){
+    // 부호 
+    if(n<0) {
+        if(n%2==0) sign=-1;
+        n*=-1;
+    }
+    else if(n==0) sign=0;
+
+    // dynamic programming
+    dp[0]=0; dp[1]=1;
+    for(int i=2; i<=n; i++) dp[i] = (dp[i-1]%MOD+dp[i-2]%MOD)%MOD;
+    if(n<0) cout << sign << "\n" << dp[n];
+    else cout << sign << "\n" << dp[n];
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> n;
+    solution();
+}
