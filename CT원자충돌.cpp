92de3cfc28594f85ab2eb8cdb05daf73ed14f65f
LIMDANBI <dlaeksql08222@gmail.com>
2022-10-06 18:27:14 +0900

    원자 충돌

--- CT원자충돌.cpp
+++ CT원자충돌.cpp
@@ -34,7 +34,7 @@ void move_atom(int y, int x){ // 원자의 이동
         int s = atom[y][x][i].s;
         int d = atom[y][x][i].d;
         int ny = (y+dy[d]*s+N*s)%N;
-        int nx = (x+dx[d]*s+m*s)%N;
+        int nx = (x+dx[d]*s+N*s)%N;
         tmp_atom[ny][nx].push_back({m,s,d});
     }
 }
