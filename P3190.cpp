cc02527a7ae4f78f7aa6e04324f0c57ef37aff13
LIMDANBI <dlaeksql08222@gmail.com>
2022-10-08 19:52:54 +0900

    뱀(구현/자료 구조/시뮬레이션/덱/큐) - 초기화 대신 +1/-1로 수정

--- P3190.cpp
+++ P3190.cpp
@@ -27,16 +27,11 @@ void input(){
         if(d=='L') info.push({t, 0}); // 'L' 외쪽으로 90'
         else info.push({t, 1}); // 'D' 오른쪽으로 90'
     }
-
-    // init
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++) snake[i][j] = EMPTY;
-    }
 }
 
 void solution(){
     int ans=0;
-    snake[0][0] = 1;
+    snake[0][0] = 2;
     while(1){
         
         ans++;
@@ -44,11 +39,11 @@ void solution(){
         // 1. 뱀 이동
         sy+=dy[dir];
         sx+=dx[dir];
-        if(sy<0 || sy>=N || sx<0 || sx>=N || snake[sy][sx]!=EMPTY) break; // 벽 또는 자기자신의 몸과 부딪히는 경우
+        if(sy<0 || sy>=N || sx<0 || sx>=N || snake[sy][sx]) break; // 벽 또는 자기자신의 몸과 부딪히는 경우
         if(apple[sy][sx]) apple[sy][sx] = 0; // 사과가 있는 경우, 사과를 먹음
         else{ // 사과가 없는 경우, 몸길이를 줄임
-            int d = snake[ey][ex];
-            snake[ey][ex] = -1;
+            int d = snake[ey][ex]-1;
+            snake[ey][ex] = 0;
             ey+=dy[d];
             ex+=dx[d];
         }
@@ -59,7 +54,7 @@ void solution(){
             else dir = (dir+4-1)%4; // 외쪽으로 90'
             info.pop();
         }
-        snake[sy][sx] = dir; // 방향 저장
+        snake[sy][sx] = dir+1; // 방향 저장 (상이 0이므로 +1 해서 저장)
     }
     cout << ans;
 }
