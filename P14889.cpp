17839ef5f8053a96bda4b0d426530a890c8d478d
LIMDANBI <dlaeksql08222@gmail.com>
2022-10-08 16:13:45 +0900

    스타트와 링크(브루트포스 알고리즘/백트래킹)

---
+++ P14889.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<vector>
+#define MAX 21
+#define INF 987654321
+using namespace std;
+
+bool is_satrt[MAX];
+int ability[MAX][MAX];
+int N, total=0, ans=INF;
+
+void input(){
+    cin >> N;
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++) {
+            cin >> ability[i][j];
+            total+=ability[i][j];
+        }
+    }
+}
+
+int get_start_score(){
+    int res = 0;
+    vector<int> v;
+    for(int i=0; i<N; i++){
+        if(is_satrt[i]) v.push_back(i);
+    }
+    for(int i=0; i<v.size()-1; i++){
+        for(int j=i; j<v.size(); j++) res+=(ability[v[i]][v[j]]+ability[v[j]][v[i]]);
+    }
+    return res;
+}
+
+int get_link_score(){
+    int res = 0;
+    vector<int> v;
+    for(int i=0; i<N; i++){
+        if(!is_satrt[i]) v.push_back(i);
+    }
+    for(int i=0; i<v.size()-1; i++){
+        for(int j=i; j<v.size(); j++) res+=(ability[v[i]][v[j]]+ability[v[j]][v[i]]);
+    }
+    return res;
+}
+
+void solution(int idx, int cnt){ // 스타트 팀과 링크 팀의 능력치의 차이의 최솟값
+    if(cnt==N/2){
+        int start = get_start_score();
+        int link = get_link_score();
+        ans = min(ans, abs(start-link));
+        return ;
+    }
+    for(int i=idx; i<N; i++){
+        is_satrt[i] = true;
+        solution(i+1, cnt+1);
+        is_satrt[i] = false;
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    input();
+    solution(0, 0);
+    cout << ans;
+}
