f30182e4e37ad9be7a1368c54302dfc64abaa0c8
LIMDANBI <dlaeksql08222@gmail.com>
2022-10-10 16:48:50 +0900

    스타트와 링크(브루트포스 알고리즘/백트래킹) - score 함수 합침

--- P14889.cpp
+++ P14889.cpp
@@ -6,57 +6,45 @@ using namespace std;
 
 bool is_satrt[MAX];
 int ability[MAX][MAX];
-int N, total=0, ans=INF;
+int N, total = 0, ans = INF;
 
-void input(){
+void input() {
     cin >> N;
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
             cin >> ability[i][j];
-            total+=ability[i][j];
+            total += ability[i][j];
         }
     }
 }
 
-int get_start_score(){
+int get_score(int who) {
     int res = 0;
     vector<int> v;
-    for(int i=0; i<N; i++){
-        if(is_satrt[i]) v.push_back(i);
+    for (int i = 0; i < N; i++) {
+        if (is_satrt[i]==who) v.push_back(i);
     }
-    for(int i=0; i<v.size()-1; i++){
-        for(int j=i; j<v.size(); j++) res+=(ability[v[i]][v[j]]+ability[v[j]][v[i]]);
+    for (int i = 0; i < v.size() - 1; i++) {
+        for (int j = i; j < v.size(); j++) res += (ability[v[i]][v[j]] + ability[v[j]][v[i]]);
     }
     return res;
 }
 
-int get_link_score(){
-    int res = 0;
-    vector<int> v;
-    for(int i=0; i<N; i++){
-        if(!is_satrt[i]) v.push_back(i);
+void solution(int idx, int cnt) { // 스타트 팀과 링크 팀의 능력치의 차이의 최솟값
+    if (cnt == N / 2) {
+        int start = get_score(1);
+        int link = get_score(0);
+        ans = min(ans, abs(start - link));
+        return;
     }
-    for(int i=0; i<v.size()-1; i++){
-        for(int j=i; j<v.size(); j++) res+=(ability[v[i]][v[j]]+ability[v[j]][v[i]]);
-    }
-    return res;
-}
-
-void solution(int idx, int cnt){ // 스타트 팀과 링크 팀의 능력치의 차이의 최솟값
-    if(cnt==N/2){
-        int start = get_start_score();
-        int link = get_link_score();
-        ans = min(ans, abs(start-link));
-        return ;
-    }
-    for(int i=idx; i<N; i++){
+    for (int i = idx; i < N; i++) {
         is_satrt[i] = true;
-        solution(i+1, cnt+1);
+        solution(i + 1, cnt + 1);
         is_satrt[i] = false;
     }
 }
 
-int main(){
+int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
     input();
     solution(0, 0);
