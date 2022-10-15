ae7d55bd7195c299575f048dcfc77a82ec8a6d17
LIMDANBI <dlaeksql08222@gmail.com>
2022-10-15 19:31:03 +0900

    어항 정리(구현/시뮬레이션)

--- P23291.cpp
+++ P23291.cpp
@@ -1,134 +1,148 @@
-#include <iostream>
-#include<cstring>
-#include <vector>
+#include<iostream>
 #include<queue>
-#define MAX 105
+#define MAX 101
+#define INF 987654321
+#define EMPTY 0
 using namespace std;
 
-struct pos{int r, c;};
+int N, K;
+int map[MAX][MAX];
+int tmp_map[MAX][MAX];
+int dy[] = { -1, 1, 0, 0 };
+int dx[] = { 0, 0, -1, 1 };
 
-int N, K, ans=0;
-int fishbowl[MAX][MAX];
-int dr[] = {-1, 1, 0, 0};
-int dc[] = {0, 0, -1, 1};
-
-void addFish(){ // 물고기 수가 가장 적은 어항에 물고기 한 마리 추가
-    vector<pos> v;
-    int min_value = 1e9;
-    for(int r=1; r<=N; r++){
-        for(int c=1; c<=N; c++){
-            if(fishbowl[r][c]==0) continue;
-            if(fishbowl[r][c] < min_value){ // 물고기 수가 더 적은 어항인 경우 
-                min_value = fishbowl[r][c]; // 최솟값 update
-                v.clear(); // 기존의 저장해둔 위치들을 비워주고,
-                v.push_back({r, c}); // 해당 최솟값의 위치 저장
-            }
-            else if(fishbowl[r][c] == min_value) v.push_back({r, c}); // 최솟값이 두개 이상일 수 o
-        }
-    }
-    for(int i=0; i<v.size(); i++) fishbowl[v[i].r][v[i].c]++; // 물고기 수가 가장 적은 어항에 물고기 한마리 씩 추가 
+void input() {
+	cin >> N >> K;
+	for (int i = 0; i < N; i++) cin >> map[N - 1][i];
 }
 
-void build(){ // 어항 쌓기
-    int pivot=1, w=1, h=1; // 기준점, 너비, 높이
-    for(int idx=0; ; idx++){
-        if(pivot-1+w+h > N) break; // 더 이상 쌓을 수 없는 경우
-        for(int c=pivot; c<pivot+w; c++){
-            for(int r=N; r>N-h; r--){ // 계방향으로 90도 회전시켜 어항을 쌓음
-                int nr = (N-w) + (c-pivot);
-                int nc = (pivot+w) + (N-r);
-                fishbowl[nr][nc] = fishbowl[r][c];
-                fishbowl[r][c] = 0;
-            }
-        }
-        pivot+=(idx/2+1); // 기준점 위치 변경
-        idx%2 ? w++ : h++; // 번갈아가며 증가하는 규칙을 가짐
-    }
+bool isContinue() {
+	int min_fish = INF, max_fish = 0;
+	for (int i = 0; i < N; i++) {
+		min_fish = min(min_fish, map[N - 1][i]);
+		max_fish = max(max_fish, map[N - 1][i]);
+	}
+	if (max_fish - min_fish <= K) return false;
+	return true;
 }
 
-void controlFish(){ // 물고기 수 조절
-    int copy_fishbowl[MAX][MAX] = {0}; // 물고기가 이동을 기록할 임시 배열
-    for(int r=1; r<=N; r++){
-        for(int c=1; c<=N; c++){
-            if(fishbowl[r][c] == 0) continue;
-            copy_fishbowl[r][c]+=fishbowl[r][c];
-            for(int i=0; i<4; i++){ // 인접한 어항에 대해, 물고기 수 차이를 구함
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-                if(fishbowl[nr][nc] == 0) continue;
-                if(fishbowl[r][c] > fishbowl[nr][nc]){
-                    int d = (fishbowl[r][c] - fishbowl[nr][nc]) / 5;
-                    copy_fishbowl[r][c]-=d; // (r, c)에서 (nr, nc)로 물고기 d 마리를 보냄
-                    copy_fishbowl[nr][nc]+=d;
-                }
-            }
-        }
-    }
-    memcpy(fishbowl, copy_fishbowl, sizeof(copy_fishbowl)); // 배열 복사
+void put_fish() {
+	int min_fish = INF;
+	for (int i = 0; i < N; i++) min_fish = min(min_fish, map[N - 1][i]);
+	for (int i = 0; i < N; i++) {
+		if (map[N - 1][i] == min_fish) map[N - 1][i]++;
+	}
 }
 
-void flatten(){ // 어항 일렬로 펼치기
-    queue<int> q;
-    for(int c=1; c<=N; c++){ // 규칙에따라 queue에 순서대로 저장
-        if(fishbowl[N][c]==0) continue;
-        for(int r=N; r>0; r--){
-            if(fishbowl[r][c]==0) break;
-            q.push(fishbowl[r][c]);
-            fishbowl[r][c] = 0;
-        }
-    }
-    int c=1;
-    while (!q.empty()){ // queue에 순서대로 저장해두었던 것을 일렬로 펼쳐줌
-        fishbowl[N][c++] = q.front();
-        q.pop();
-    }
+void fold_fish() {
+	int start = 0, w = 1, h = 1;
+	for (int idx = 0; ; idx++) {
+
+		if (start + w + h - 1 >= N) break; // 더 이상 어항을 쌓을 수 없는 경우
+		
+		for (int x = start; x < start + w; x++) {
+			for (int y = N - 1; y > N - 1 - h; y--) {
+				int ny = N - 1 - w + x - start;
+				int nx = start + w + N - 1 - y;
+				map[ny][nx] = map[y][x];
+				map[y][x] = EMPTY;
+			}
+		}
+		start += (idx / 2 + 1);  // 시작점 위치
+		idx % 2 ? w++ : h++; // 번갈아가며 증가
+	}
 }
 
-void fold(){ // 어항 2번 접기
-    int num = N/4;
-    int col[4] = {0, N, N-num+1, N}; // 옮겨지는 시작 위치
-    int dir[4] = {0, -1, 1, -1}; // 1 : -> , -1 : <-
+void control_fish() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (map[i][j] == EMPTY) continue;
 
-    int idx = 1;
-    for(int i=1; i<4; i++){
-        int c = col[i];
-        for(int j=0; j<num; j++){
-            fishbowl[N-i][c] = fishbowl[N][idx];
-            fishbowl[N][idx] = 0;
-            c+=dir[i]; idx++;
-        }
-    }
+			for (int d = 0; d < 4; d++) {
+				int ny = i + dy[d];
+				int nx = j + dx[d];
+				if (map[ny][nx] == EMPTY) continue;
+				int fish_num = map[i][j];
+				if (map[ny][nx] < fish_num) {
+					fish_num -= map[ny][nx];
+					fish_num /= 5;
+					tmp_map[ny][nx] += fish_num;
+					tmp_map[i][j] -= fish_num;
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			map[i][j] += tmp_map[i][j];
+			tmp_map[i][j] = EMPTY;
+		}
+	}
 }
 
-int diff(){ // max - min 값 반환
-    int max_value = 0;
-    int min_value = 1e9;
-    for(int r=1; r<=N; r++){
-        for(int c=1; c<=N; c++){
-            if(fishbowl[r][c]==0) continue;
-            max_value = max(max_value, fishbowl[r][c]);
-            min_value = min(min_value, fishbowl[r][c]);
-        }
-    }
-    return max_value-min_value;
+void spread_fihs() {
+
+	queue<int> q;
+	for (int col = 0; col < N; col++) {
+		for (int row = N - 1; row >= 0; row--) {
+			if (map[row][col]) {
+				q.push(map[row][col]);
+				map[row][col] = EMPTY;
+			}
+			else break;
+		}
+	}
+
+	int col = 0;
+	while (!q.empty() && col<N) {
+		map[N - 1][col++] = q.front();
+		q.pop();
+	}
 }
 
-int main(){
-    ios_base::sync_with_stdio(0); cin.tie(0);
-    cin >> N >> K;
-    for(int c=1; c<=N; c++) cin >> fishbowl[N][c]; // 물고기 수 입력
-    while (1){ // max-min <= K 일 때까지 어항 정리
-        if(diff() <= K) break;
-        addFish();
+void two_fold_fish() {
+	for (int col = 0; col < N / 2; col++) {
+		map[N - 2][N - col - 1] = map[N - 1][col];
+		map[N - 1][col] = EMPTY;
+	}
+	for (int col = N / 2; col < N / 2 + N / 4; col++) {
+		map[N - 4][N - col + N / 2 - 1] = map[N - 1][col];
+		map[N - 1][col] = EMPTY;
+		map[N - 3][N - col + N / 2 - 1] = map[N - 2][col];
+		map[N - 2][col] = EMPTY;
+	}
+}
 
-        build();
-        controlFish();
-        flatten();
-        
-        fold();
-        controlFish();
-        flatten();
-        
-        ans++;
-    } cout << ans;
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	input();
+
+	int ans = 0;
+	while (isContinue()) {
+
+		// 1. 물고기의 수가 가장 적은 어항에 물고기를 한 마리 넣음
+		put_fish();
+
+		// 2. 어항을 쌓음
+		fold_fish();
+
+		// 3. 어항에 있는 물고기의 수 조절
+		control_fish();
+
+		// 4. 어항을 바닥에 일렬로 놓음
+		spread_fihs();
+
+		// 5. 가운데를 중심으로 왼쪽 N/2개를 공중 부양
+		two_fold_fish();
+
+		// 6. 다시 물고기 수 조절 
+		control_fish();
+
+		// 7. 다시 바닥에 일렬로 놓음
+		spread_fihs();
+
+		ans++;
+	}
+	cout << ans;
 }
