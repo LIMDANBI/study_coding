a3beb0519c9855a097e82b49d61305430c8cd82e
LIMDANBI <dlaeksql08222@gmail.com>
2022-11-01 10:02:11 +0900

    카드 제거

---
+++ swea14557.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int T; cin >> T;
+    for(int t=1; t<=T; t++){
+        string S; cin >> S;
+
+        int cnt = 0;
+        for(int i=0; i<S.size(); i++){
+            if(S[i]=='1') cnt++;
+        }
+
+        if(cnt%2) cout << "#" << t << " yes\n";
+        else cout << "#" << t << " no\n";
+    }
+}
